1fe1c5f1cd8985ed1025546be3d0290a0547978f
U-B-O-T-S <shreyaskhorgade@gmail.com>
2020-05-22 15:10:09 +0530

    Minor edits

--- Module4-Solution/SpeakGoodbye.js
+++ Module4-Solution/SpeakGoodbye.js
@@ -1,11 +1,13 @@
-(function(window){
+(function(window)
+ {
     var byeSpeaker={};
     var speakWord = "Good Bye";
-    byeSpeaker.speak = function(name) {
-    console.log(speakWord + " " + name);
-}
+    byeSpeaker.speak = function(name) 
+     {
+      console.log(speakWord + " " + name);
+     }
 
 
- window.byeSpeaker = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 
 })(window);
