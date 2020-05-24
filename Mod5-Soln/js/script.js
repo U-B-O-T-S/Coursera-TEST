21bb7d97f77d2c9228e4abe7d60d218dd7c70e5f
U-B-O-T-S <shreyaskhorgade@gmail.com>
2020-05-24 18:13:16 +0530

    final touch

--- Mod5-Soln/js/script.js
+++ Mod5-Soln/js/script.js
@@ -114,8 +114,8 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'"
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chooseRandomCategory)
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chooseRandomCategory);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
